3b286ab3c27bbecd56f33353036fcfa10d3275fc
macykozak <macykozak@gmail.com>
2021-05-08 06:40:28 -0500

    Seeded Seeds File with Artificial Data)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flower.create(color: "Pink", price: 30.00)
